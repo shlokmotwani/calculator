759650e7e28d3844f26401aa4bffe6002a3c1be7
Shlok Motwani <shlok.codes@gmail.com>
2024-06-29 17:16:29 +0530

    Cleaned code. Improved functionality.
    Need to improve functionality of = button

--- javascript.js
+++ javascript.js
@@ -1,192 +1,159 @@
-function add(a, b){
-    return +a + +b;
-}
-
-function subtract(a, b){
-    return +a - +b;
-}
-
-function multiply(a, b){
-    return +a * +b;
-}
-
-function divide(a, b){
-    return +a / +b;
-}
-
-function operate(a, operator, b){
-    let answer = 0;
-    switch(operator){
-        case "+":
-            answer = add(a,b);
-            break;
-        case "-":
-            answer = subtract(a,b);
-            break;
-        case "*":
-            answer = multiply(a,b);
-            break;
-        case "/":
-            answer = divide(a,b);
-            break;
-    }
-    return answer;
-}
-
+const outputField = document.querySelector("#output-field");
 const divButtons = document.querySelector("#buttons");
 const divDigits = document.querySelector("#digits");
-const outputField = document.querySelector("#output-field");
-outputField.value = 0;
 const divOperators = document.querySelector("#operators");
-createDigitButtons(divDigits);
-createOperatorButtons(divOperators);
+const equalsButton = document.createElement("button");
+const op_symbols = ["+", "-", "*", "/", "CLEAR"];
 
-let equalsButton = document.createElement("button");
+outputField.value = 0;
 equalsButton.textContent = "=";
 equalsButton.style.width = "93%";
-divDigits.appendChild(equalsButton);
 
+divOperators.setAttribute(
+  "style",
+  `display: flex;
+  flex-direction: column;`
+);
 
-divOperators.setAttribute("style",
-`display: flex;
-flex-direction: column;`);
+function add(a, b) {
+  return +a + +b;
+}
 
+function subtract(a, b) {
+  return +a - +b;
+}
 
+function multiply(a, b) {
+  return +a * +b;
+}
 
-function createDigitButtons(parent){
-    let digitButtonArray = [];
-    for(let i=0; i<10; i++){
-        digitButtonArray[i] = document.createElement("button");
-        digitButtonArray[i].textContent = i;
-        digitButtonArray[0].style.width = "93%";
+function divide(a, b) {
+  return +a / +b;
+}
 
+function operate(a, operator, b) {
+  let answer = 0;
+  switch (operator) {
+    case "+":
+      answer = add(a, b);
+      break;
+    case "-":
+      answer = subtract(a, b);
+      break;
+    case "*":
+      answer = multiply(a, b);
+      break;
+    case "/":
+      answer = divide(a, b);
+      break;
+  }
+  return answer;
+}
+
+function createDigitButtons(parent) {
+  let digitButtonsArray = []; //stores all the digit buttons
+  let digitButtonRowsArray = []; //stores all the button rows
+
+  //creates digit buttons from 0-9 and pushes them into digitButtonsArray
+  for (let i = 0; i < 10; i++) {
+    let btn = document.createElement("button");
+    btn.textContent = i;
+    digitButtonsArray.push(btn);
+  }
+
+  digitButtonsArray[0].style.width = "93%";
+
+  for (let i = 0; i < 4; i++) {
+    digitButtonRowsArray.push(document.createElement("div"));
+  }
+
+  let count = 0;
+  for (let i = 0; i < 4; i++) {
+    digitButtonRowsArray[i].appendChild(digitButtonsArray[count++]);
+    if (i > 0) {
+      digitButtonRowsArray[i].appendChild(digitButtonsArray[count++]);
+      digitButtonRowsArray[i].appendChild(digitButtonsArray[count++]);
+    }
+  }
+
+  for (let i = 3; i >= 0; i--) {
+    parent.appendChild(digitButtonRowsArray[i]);
+  }
+}
+
+function createOperatorButtons(parent) {
+  for (let i = 0; i < op_symbols.length; i++) {
+    let btn = document.createElement("button");
+    btn.textContent = op_symbols[i];
+    parent.appendChild(btn);
+  }
+}
+
+function runCalculator() {
+  addEventListenersToButtons(divButtons);
+}
+
+function addEventListenersToButtons(parent) {
+  let firstNum = 0;
+  let secondNum = 0;
+  let currentOperator = "";
+  let isOperatorPressed = false;
+  let result = 0;
+
+  parent.addEventListener("click", (e) => {
+    let buttonText = e.target.textContent;
+    if (!String(e.target).includes("Button")) {
+      return;
     }
 
-    const row0 = document.createElement("div");
-    const row1 = document.createElement("div");
-    const row2 = document.createElement("div");
-    const row3 = document.createElement("div");
+    if (buttonText == "CLEAR") {
+      firstNum = null;
+      secondNum = null;
+      currentOperator = "";
+      isOperatorPressed = false;
+      result = 0;
+      outputField.value = 0;
+    } else if (op_symbols.includes(buttonText)) {
+      if (!isOperatorPressed) {
+        if (firstNum) {
+          secondNum = +outputField.value;
+          result = operate(firstNum, currentOperator, secondNum);
+          firstNum = result;
+          secondNum = null;
+          outputField.value = result;
+          currentOperator = "";
+        } else {
+          firstNum = +outputField.value;
+        }
+      }
+      isOperatorPressed = true;
+      currentOperator = buttonText;
+    } else if (buttonText == "=") {
+      secondNum = +outputField.value;
 
-    row0.appendChild(digitButtonArray[0]);
-    row1.appendChild(digitButtonArray[1]);
-    row1.appendChild(digitButtonArray[2]);
-    row1.appendChild(digitButtonArray[3]);
-    row2.appendChild(digitButtonArray[4]);
-    row2.appendChild(digitButtonArray[5]);
-    row2.appendChild(digitButtonArray[6]);
-    row3.appendChild(digitButtonArray[7]);
-    row3.appendChild(digitButtonArray[8]);
-    row3.appendChild(digitButtonArray[9]);
-
-    parent.appendChild(row3);
-    parent.appendChild(row2);
-    parent.appendChild(row1);
-    parent.appendChild(row0);
-
+      result = operate(firstNum, currentOperator, secondNum);
+      isOperatorPressed = false;
+      currentOperator = "";
+      outputField.value = result;
+      firstNum = secondNum;
+      secondNum = null;
+    } else {
+      if (outputField.value == 0) {
+        outputField.value = buttonText;
+      } else {
+        if (isOperatorPressed) {
+          outputField.value = buttonText;
+          isOperatorPressed = false;
+        } else {
+          outputField.value += buttonText;
+        }
+      }
+    }
+  });
 }
 
-function createOperatorButtons(parent){
-    let addButton = document.createElement("button");
-    addButton.textContent = "+";
-    let subtractButton = document.createElement("button");
-    subtractButton.textContent = "-";
-    let multiplyButton = document.createElement("button");
-    multiplyButton.textContent = "*";
-    let divideButton = document.createElement("button");
-    divideButton.textContent = "/";
-    let clearButton = document.createElement("button");
-    clearButton.textContent = "CLEAR";
-
-    parent.appendChild(addButton);
-    parent.appendChild(subtractButton);
-    parent.appendChild(multiplyButton);
-    parent.appendChild(divideButton);
-    parent.appendChild(clearButton);
-}
-
-function runCalculator(){
-    addEventListenersToButtons(divButtons);
-}
-
-function addEventListenersToButtons(parent){
-    let firstNum = 0;
-    let secondNum = 0;
-    let currentOperator = "";
-    let isOperatorPressed = false;
-    let result = 0;
-
-    parent.addEventListener("click", (e)=>{
-        let buttonText = e.target.textContent;
-        if(!String(e.target).includes("Button")){
-            return;
-        }
-        
-        if(buttonText == "CLEAR"){
-            firstNum = null;
-            secondNum = null;
-            currentOperator = "";
-            isOperatorPressed = false;
-            result = 0;
-            outputField.value = 0;
-        }
-        else if(buttonText == "+" ||
-            buttonText == "-" ||
-            buttonText == "*" || 
-            buttonText == "/"){
-            
-            isOperatorPressed = true;
-
-            if(firstNum && secondNum){
-                result = operate(firstNum, currentOperator, secondNum);
-                firstNum = result;
-                outputField.value = firstNum;
-                currentOperator = "";
-                isOperatorPressed = false;
-            }
-            else{
-                firstNum = outputField.value;
-            }
-            currentOperator = buttonText;
-
-            /*console.log(`isOperatorPressed: ${isOperatorPressed}
-            firstNum: ${firstNum}
-            secondNum: ${secondNum}`);*/
-            
-        }
-        else if(buttonText == "="){
-            secondNum = outputField.value;
-
-            /*console.log(`---------BEFORE = OPERATIONS--------------
-            firstNum:  ${firstNum}
-            secondNum:  ${secondNum}
-            outputField.value: ${outputField.value}
-            isOperatorPressed: ${isOperatorPressed}`);*/
-
-            result = operate(firstNum, currentOperator, secondNum);
-            isOperatorPressed = false;
-            outputField.value = result;
-
-            /*console.log(`---------AFTER = OPERATIONS--------------
-            firstNum:  ${firstNum}
-            secondNum:  ${secondNum}
-            outputField.value: ${outputField.value}
-            isOperatorPressed: ${isOperatorPressed}`);*/
-        }
-        else{
-            if(outputField.value == 0){
-                outputField.value = buttonText;
-            }
-            else{
-                outputField.value += buttonText;
-            }
-
-            if(isOperatorPressed){
-                outputField.value = buttonText;
-                isOperatorPressed = false;
-            }
-        }
-    });
-}
-
+createDigitButtons(divDigits);
+createOperatorButtons(divOperators);
+divDigits.appendChild(equalsButton);
 
 runCalculator();
